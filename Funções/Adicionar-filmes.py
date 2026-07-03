d0044e2ffde42954f9fd6fc5ca8469298a9be5df
Matheus-Nere <nerematheus50@gmail.com>
2026-07-03 08:03:25 -0300

    add initial film addition functionality and user option menu

---
+++ Funções/Adicionar-filmes.py
@@ -0,0 +1,2 @@
+function adicionarFilmes (filme):
+    jsonFilme = {nome: ?}
---
+++ rascunho.py
@@ -0,0 +1,10 @@
+print("NEREFLIX\n")
+
+option = int(input("escolha uma opção:\n"))
+
+print(
+"Opção 1 - Adicionar Filme:\n " \
+"Opção 2 - Modificar Filme:\n"  \
+"Opção 3 - Buscar Filme:\n" \
+"Opção 4 - Excluir Filme:\n" \
+"Opção 3 - Buscar Filme:\n" )
