dff48a3460e43737f2c81e54840881a0a64ace19
Matheus-Nere <nerematheus50@gmail.com>
2026-07-03 14:09:44 -0300

    add functionality to save films to JSON in adicionar_filmes.py

--- Funções/adicionar_filmes.py
+++ Funções/adicionar_filmes.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 def adicionarFilmes ():
     filme = {"nome": 0, "diretor": 0, "genero": 0, "ano": 0}
     fnome = input("insira o nome do filme:\n")
@@ -8,5 +11,17 @@ def adicionarFilmes ():
     filme["diretor"] = fdiretor
     filme["genero"] = fgenero
     filme["ano"] = fano
-    return filme
-import Funções.adicionar_filmes as adicionar_filmes
+    if os.path.exists("Catalogo.lista_filmes.json"):
+     with open("Catalogo.lista_filmes.json", "r", encoding="utf-8") as filme:
+        dados_existentes = json.load(filme)
+    else:
+        dados_existentes = {}
+
+    dados_existentes.update(filme)
+
+    with open("Catalogo.lista_filmes.json", "w", encoding="utf-8") as filme:
+        json.dump(dados_existentes, filme, ensure_ascii=False, indent=4)
+
+adicionarFilmes()
+
+#import Funções.adicionar_filmes as adicionar_filmes
