cb021f79087713d2628010431056d45b19d59c22
Matheus-Nere <nerematheus50@gmail.com>
2026-07-14 13:05:35 -0300

    remove sample film data from lista_filmes.json and refactor rascunho.py for improved menu structure

--- rascunho.py
+++ rascunho.py
@@ -1,14 +1,24 @@
+import os
+import time
 from Funcoes.adicionar_filmes import adicionarFilmes
-print("NEREFLIX\n")
 
-print(
-"Opção 1 - Adicionar Filme:\n " \
-"Opção 2 - Modificar Filme:\n"  \
-"Opção 3 - Buscar Filme:\n" \
-"Opção 4 - Excluir Filme:\n" \
-"Opção 3 - Buscar Filme:\n" )
+def menu ():
+    print("NEREFLIX\n")
+    print(
+    "Opção 1 - Adicionar Filme\n " \
+    "Opção 2 - Modificar Filme\n"  \
+    "Opção 3 - Buscar Filme\n" \
+    "Opção 4 - Excluir Filme\n" \
+    "Opção 5 - sair\n" )
 
+menu()
 option = int(input("escolha uma opção:\n"))
 
 if option == 1:
     adicionarFilmes()
+    print("filme adicionado")
+    time.sleep(3)
+    os.system("clear")
+
+if option == 5:
+    os.system("clear")
