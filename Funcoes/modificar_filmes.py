6c82562c96292f756b2318b69203253356366a85
Matheus-Nere <nerematheus50@gmail.com>
2026-07-24 14:11:33 -0300

    refactor modificar_filmes.py to enhance film modification logic and improve user prompts

--- Funcoes/modificar_filmes.py
+++ Funcoes/modificar_filmes.py
@@ -4,23 +4,20 @@ pasta_script = os.path.dirname(os.path.abspath(__file__))
 caminho_json = os.path.join(pasta_script, "..", "Catalogo", "lista_filmes.json")
 caminho_json = os.path.abspath(caminho_json)
 
-def listafilmes(filme):
+def modificar_filmes(filme):
     with open (caminho_json, "r", encoding="utf-8") as arquivo:
         dados = json.load(arquivo)
         for i in dados:
-            if i['nome'] == filme:
-                print (i)
+            if i['nome'].lower() == filme.lower():
                 print()
-                opcao = input("Filme econtrado, escolha a informação para modificar")
-                if opcao == "nome":
-                    print()
-                if opcao == "diretor":
-                    print()
-                if opcao == "genero":
-                    print()
-                if opcao == "ano":
-                    print()
-                else:
-                    print("Opção não encontrada, tente novamente")
-        print("filme não encontrado")
+                print (i)
+                print("\nfilme encontrado, iniciando modificações, mantenha em branco caso queira manter a informação.\n")
+                for chave in i:
+                    novo_valor = str(input(f"Insira o novo {chave} do filme: \n"))
+                    i[chave] = novo_valor
+                break
+            else: 
+                print("filme não encontrado")
+                
 
+modificar_filmes('mari')
