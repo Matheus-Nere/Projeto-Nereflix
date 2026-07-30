5be22cffec640a3eec7ff2737692041e64019d81
Matheus-Nere <nerematheus50@gmail.com>
2026-07-30 13:28:13 -0300

    refactor modificar_filmes.py to streamline film modification process and remove teste.py

--- Funcoes/modificar_filmes.py
+++ Funcoes/modificar_filmes.py
@@ -3,22 +3,32 @@ import json
 pasta_script = os.path.dirname(os.path.abspath(__file__))
 caminho_json = os.path.join(pasta_script, "..", "Catalogo", "lista_filmes.json")
 caminho_json = os.path.abspath(caminho_json)
+def achar (dados, filme):
+    
+    for i in dados:
+        if i['nome'].lower() == filme.lower():
+           
+            filme_achado = i
+            return filme_achado
+            
+        
+def modificar(i):
+    print()
+    print (i)
+    print("\nfilme encontrado, iniciando modificações, mantenha em branco caso queira manter a informação.\n")
+    for chave in i:
+        novo_valor = str(input(f"Insira o novo {chave} do filme: \n"))
+        if novo_valor:
+            i[chave] = novo_valor
+       
 
 def modificar_filmes(filme):
     with open (caminho_json, "r", encoding="utf-8") as arquivo:
         dados = json.load(arquivo)
-        for i in dados:
-            if i['nome'].lower() == filme.lower():
-                print()
-                print (i)
-                print("\nfilme encontrado, iniciando modificações, mantenha em branco caso queira manter a informação.\n")
-                for chave in i:
-                    novo_valor = str(input(f"Insira o novo {chave} do filme: \n"))
-                    i[chave] = novo_valor
-                break
-            else: 
-                print("filme não encontrado")
-
-                
-
-modificar_filmes('mari')
+        filme_achado = achar(dados, filme)
+        if filme_achado and filme_achado["nome"].lower() == filme.lower():
+            modificar(filme_achado)
+            with open(caminho_json, "w", encoding="utf-8") as arquivo:
+                json.dump(dados, arquivo, ensure_ascii=False, indent=4)
+        else:
+            print("filme não encontrado")
